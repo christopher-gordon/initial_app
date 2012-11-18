509789cc9669ee8f10448c312f795468a55e7d51
Chris Gordon <ChristopherScottGordon@gmail.com>
2012-11-18 17:07:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concrit::Application.config.secret_token = '59150ca41e8402ac37de30e5b5efa378f74d03d930a250db311846f8e578c0c0ef0cf5504d65c590f89883347a8ef56e4509427a8214dce86bbcdeae48a946a0'
